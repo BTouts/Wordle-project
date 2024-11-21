8476f99a5cf25312143bfd196fb3b4ba06eb9a00
Brett Toutkoushian <bt03850@uga.edu>
2024-11-20 21:48:15 -0500

    updating ports

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ def evaluate_guess(secret_word, user_guess):
     return result
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
